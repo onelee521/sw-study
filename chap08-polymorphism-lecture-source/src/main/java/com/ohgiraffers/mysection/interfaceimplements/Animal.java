e72c71d3d2521841124307a361800ad98ece7e4e
oneye <onelee521@gmail.com>
2024-01-22 16:52:56 +0900

    [update] 스터디 후 코드 수정

--- chap08-polymorphism-lecture-source/src/main/java/com/ohgiraffers/mysection/interfaceimplements/Animal.java
+++ chap08-polymorphism-lecture-source/src/main/java/com/ohgiraffers/mysection/interfaceimplements/Animal.java
@@ -6,6 +6,6 @@ public interface Animal {
     void eat();
 
     String getFeed();
-    // get set 사용 불가능? -> 생성자도 없고 final이라서 그런가?
+    // get set 사용 불가능?
     // getter 사용 가능, setter는 불가능이지만 자바8부터 default 메소드로 선언가능
 }
--- chap08-polymorphism-lecture-source/src/main/java/com/ohgiraffers/mysection/interfaceimplements/Mammalia.java
+++ chap08-polymorphism-lecture-source/src/main/java/com/ohgiraffers/mysection/interfaceimplements/Mammalia.java
@@ -1,14 +1,14 @@
 package com.ohgiraffers.mysection.interfaceimplements;
 
 public interface Mammalia extends Animal{
-    String BED = "대나무 침대";
+    String BED = "침대";
 //    @Override
 //    void eat();           //인터페이스끼리는 오버라이딩 안해도 됨.
     void sleep();
 
 
     public default void sleepbed() {
-        System.out.println("판다가 " + BED + "에서 잡니다.");
+        System.out.println(BED + "에서 잡니다.");
     }
 
     // Mammalia.sleepbed만 가능
