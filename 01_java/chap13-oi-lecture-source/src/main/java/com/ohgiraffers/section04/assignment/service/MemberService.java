8d730fc6d29bce44a351d60b52f2a6b48b33135a
oneye <onelee521@gmail.com>
2024-01-25 15:35:05 +0900

    [update] 회원관리 프로그램 2번까지 추가

---
+++ 01_java/chap13-oi-lecture-source/src/main/java/com/ohgiraffers/section04/assignment/service/MemberService.java
@@ -0,0 +1,32 @@
+package com.ohgiraffers.section04.assignment.service;
+
+import com.ohgiraffers.section04.assignment.aggregate.Member;
+import com.ohgiraffers.section04.assignment.repository.MemberRepository;
+
+import java.util.ArrayList;
+
+/* 설명. 트랜젝션 성공실패 여부 확인 및 회원 관련 비즈니스 로직을 처리하는 클래스 */
+public class MemberService {
+
+    private final MemberRepository mr = new MemberRepository();
+
+    public void selectAllMembers() {
+        ArrayList<Member> selectedMembers = mr.selectAllMembers();
+
+//        System.out.println("=== service까지 잘 반환되어 오나 확인 ===");
+//        for(Member m: selectedMembers) {
+//            System.out.println(m);
+//        }
+    }
+
+    /* 설명. 전달된 회원 번호를 활용해 repository에 있는 memberList로부터 해당 회원을 찾아 반환 받기 */
+    public void selectMember(int memNo) {
+        Member selectedMember = mr.selectMember(memNo);
+
+        if(selectedMember == null) {
+            System.out.println("해당 회원이 존재하지 않습니다.");
+        } else {
+            System.out.println("조회된 회원은: " + selectedMember);
+        }
+    }
+}
