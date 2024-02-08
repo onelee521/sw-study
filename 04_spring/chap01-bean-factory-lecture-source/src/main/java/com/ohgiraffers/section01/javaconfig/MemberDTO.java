0402300905618f1452828f30f62654ddbe01731f
oneye <onelee521@gmail.com>
2024-02-08 16:16:28 +0900

    spring bean 추가

---
+++ 04_spring/chap01-bean-factory-lecture-source/src/main/java/com/ohgiraffers/section01/javaconfig/MemberDTO.java
@@ -0,0 +1,16 @@
+package com.ohgiraffers.section01.javaconfig;
+
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@ToString
+public class MemberDTO {
+    private int sequence;
+    private String id;
+    private String pwd;
+    private String name;
+
+}
