221df5b9d3d6401d4cd479e437cecd7a3ed03241
oneye <onelee521@gmail.com>
2024-02-15 15:57:29 +0900

    request mapping, handler 추가

---
+++ 05_springboot/chap03-handler-method-lecture-soure/src/main/java/com/ohgiraffers/handlermethod/Chap03HandlerMethodLectureSoureApplication.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.handlermethod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chap03HandlerMethodLectureSoureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Chap03HandlerMethodLectureSoureApplication.class, args);
+    }
+
+}
