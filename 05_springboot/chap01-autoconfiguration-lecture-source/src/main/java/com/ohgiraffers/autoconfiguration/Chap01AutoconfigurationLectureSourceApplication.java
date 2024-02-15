9ecb8911c5adfa5b63e703ff8e51c92ba5a544fb
oneye <onelee521@gmail.com>
2024-02-15 12:35:22 +0900

    스프링부트의 전반적이 설명과 야믈과 같은 설정 파일을 불러오는 방법 추가

---
+++ 05_springboot/chap01-autoconfiguration-lecture-source/src/main/java/com/ohgiraffers/autoconfiguration/Chap01AutoconfigurationLectureSourceApplication.java
@@ -0,0 +1,12 @@
+package com.ohgiraffers.autoconfiguration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chap01AutoconfigurationLectureSourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chap01AutoconfigurationLectureSourceApplication.class, args);
+	}
+}
