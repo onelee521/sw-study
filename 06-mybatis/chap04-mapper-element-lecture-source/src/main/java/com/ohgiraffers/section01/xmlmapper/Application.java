e25dcb95d4117084465c52269fbc34fbfbdf580a
oneye <onelee521@gmail.com>
2024-02-22 14:04:23 +0900

    mapper element 추가

---
+++ 06-mybatis/chap04-mapper-element-lecture-source/src/main/java/com/ohgiraffers/section01/xmlmapper/Application.java
@@ -0,0 +1,47 @@
+package com.ohgiraffers.section01.xmlmapper;
+
+import java.util.Scanner;
+
+public class Application {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        do {
+            System.out.println("========= 매퍼 elements 테스트 메뉴 =========");
+            System.out.println("1. <resultMap 테스트>");
+            System.out.print("메뉴번호를 입력하세요: ");
+            int no = sc.nextInt();
+
+            switch (no) {
+                case 1:
+                    resultMapSubMenu();
+                    break;
+            }
+        } while (true);
+    }
+
+    private static void resultMapSubMenu() {
+        Scanner sc = new Scanner(System.in);
+        ElementService elementService = new ElementService();
+        do {
+            System.out.println("======== <result> 서브메뉴 ========");
+            System.out.println("1. <resultMap> 테스트");
+            System.out.println("2. <association> 테스트");
+            System.out.println("3. <collection> 테스트");
+            System.out.print("메뉴 번호를 입력하세요: ");
+            int no = sc.nextInt();
+
+            switch (no) {
+                case 1:
+                    elementService.selectResultMapTest();
+                    break;
+                case 2:
+                    elementService.selectResultMapAssociationTest();
+                    break;
+                case 3:
+                    elementService.selectResultMapCollectionTest();
+                    break;
+
+            }
+        } while (true);
+    }
+}
