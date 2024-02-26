a3601be0413e229f9d35feaf89af5ea934a12e5e
oneye <onelee521@gmail.com>
2024-02-26 11:31:45 +0900

    jpa를 사용해야하는 이유 추가

--- 08_jpa/chap00-intro-lecture-source/src/test/java/com/ohgiraffers/section01/problem/ProblemOfUsingDirectSQLTests.java
+++ 08_jpa/chap00-intro-lecture-source/src/test/java/com/ohgiraffers/section01/problem/ProblemOfUsingDirectSQLTests.java
@@ -37,7 +37,7 @@ public class ProblemOfUsingDirectSQLTests {
      *  2. sql에 의존하여 개발
      *  3. 패러다임 불일치(상속, 연관관계, 객체 그래프 탐색, 방향성)
      *  4. 동일성 보장 문제
-    * */
+     * */
 
     /* 설명. 1. 데이터 변환, spl 작성, jdbc api 코드 등의 중복 작성(개발시간 증가, 유지보수성 저하) */
     @DisplayName("직접 SQL을 작성하여 메뉴를 조회할 때 발생하는 문제 확인")
@@ -172,4 +172,73 @@ public class ProblemOfUsingDirectSQLTests {
     }
 
     /* 설명. 3. 패러다임 불일치(상속, 연관관계, 객체 그래프 탐색, 방향성) */
+    /* 설명. 3-1. 상속 문제
+     *  객체 지향 언어의 상속 개념과 유사한 것이 데이터베이스의 서브타입엔티티이다. (서브타입의 별도 클래스로 나뉘었을 때)
+     *  슈퍼타입의 모든 속성을 서브타입이 공유하지 못하여 물리적으로 다른 테이블로 분리된 형태이다.
+     *  (설계에 따라서는 하나의 테이블로 속성이 추가되기도 한다.)
+     *  하지만 객체지향의 상속은 슈퍼타입의 속성을 공유해서 사용하므로 여기에서 패러다임의 불일치가 발생한다.
+     * */
+
+    /* 설명. 3-2. 연관관계문제, 객체 그래프 탐색 문제, 방향성 문제
+     *  객체지향에서 말하는 가지고 있는 (ASSOCIATION 연관관계 혹은 COLLECTION 연관관계) 경우 데이터베이스 저장구조와 다른형태이다.
+     *
+     * 설명.
+     *  - 데이터베이스 테이블에 맞춘 객체 모델
+     *  public class Menu {
+     *   private int menuCode;
+     *   private String menuName;
+     *   private int menuPrice;
+     *   private int categoryCode; //
+     *   private String orderbleStatus;
+     *  }
+     *  - 객체 지향 언어에 맞춘 객체 모델
+     *  public class Menu {
+     *   private int menuCode;
+     *   private String menuName;
+     *   private int menuPrice;
+     *   private Category category; // 이렇게 코드를 짤 수 있음 항상 조인이 일어날 때(중간객체 등), VO라고 함
+     *   private String orderbleStatus;
+     *  }
+     * */
+
+    /* 설명. 4. 동일성 보장 문제 */
+    @DisplayName("조회한 두 개의 행을 담은 객체의 동일성 비교 테스트")
+    @Test
+    void testEqual() throws SQLException {
+
+        //given
+        String query = "SELECT MENU_CODE, MENU_NAME FROM TBL_MENU WHERE MENU_CODE = 12";
+
+        //when
+        Statement stmt1 = con.createStatement();
+        ResultSet rset1 = stmt1.executeQuery(query);
+
+        Menu menu1 = null;
+
+        while ((rset1.next())) {
+            menu1 = new Menu();
+            menu1.setMenuCode(rset1.getInt("MENU_CODE"));
+            menu1.setMenuName(rset1.getString("MENU_NAME"));
+        }
+
+        Statement stmt2 = con.createStatement();
+        ResultSet rset2 = stmt2.executeQuery(query);
+
+        Menu menu2 = null;
+
+        while ((rset2.next())) {
+            menu2 = new Menu();
+            menu2.setMenuCode(rset2.getInt("MENU_CODE"));
+            menu2.setMenuName(rset2.getString("MENU_NAME"));
+        }
+
+        // then
+        Assertions.assertNotEquals(menu1, menu2); // 다른 객체일 때 테스트 통과
+    }
+
+    /* 설명. JPA를 이용하면 동일 비교가 가능
+     *  Menu menu1 = entityManager.find(Menu.class, 12);
+     *  Menu menu2 = entityManager.find(Menu.class, 12);
+     *  System.out.println(menu1 == menu2); // true(동일성이 보장, 같은 객체)
+     * */
 }
