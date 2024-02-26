7573514e7434c40f239a4e02f8c2f0500e87904e
oneye <onelee521@gmail.com>
2024-02-26 10:51:06 +0900

    jdbc api를 이용하여 sql을 다룰 때 문제점 2번까지 업로드

---
+++ 08_jpa/chap00-intro-lecture-source/src/test/java/com/ohgiraffers/section01/problem/ProblemOfUsingDirectSQLTests.java
@@ -0,0 +1,175 @@
+package com.ohgiraffers.section01.problem;
+
+import org.junit.jupiter.api.*;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProblemOfUsingDirectSQLTests {
+
+    /* 설명. jdbcTemplate의 ... 하는 역할 */
+
+    private Connection con;
+
+    @BeforeEach
+    void setConnection() throws ClassNotFoundException, SQLException {
+        String driver = "com.mysql.cj.jdbc.Driver";
+        String url = "jdbc:mysql://localhost:3306/menudb";
+        String user = "swcamp";
+        String password = "swcamp";
+
+        Class.forName(driver);
+
+        con = DriverManager.getConnection(url, user, password);
+        con.setAutoCommit(false);
+    }
+
+    @AfterEach
+    void closeConnection() throws SQLException {
+        con.rollback();     // 테스트만하고 디비에 적용x
+        con.close();
+    }
+
+    /*  */
+    /* 설명. JDBC API를 이용해 직접 spl을 다룰 때 발생할 수 있는 문제점
+     *  1. 데이터 변환, spl 작성, jdbc api 코드 등의 중복 작성(개발시간 증가, 유지보수성 저하)
+     *  2. sql에 의존하여 개발
+     *  3. 패러다임 불일치(상속, 연관관계, 객체 그래프 탐색, 방향성)
+     *  4. 동일성 보장 문제
+    * */
+
+    /* 설명. 1. 데이터 변환, spl 작성, jdbc api 코드 등의 중복 작성(개발시간 증가, 유지보수성 저하) */
+    @DisplayName("직접 SQL을 작성하여 메뉴를 조회할 때 발생하는 문제 확인")
+    @Test
+    void testDirectSelectSql() throws SQLException {
+
+        //given
+        String query = "SELECT MENU_CODE, MENU_NAME, MENU_PRICE, CATEGORY_CODE, " +
+                "ORDERABLE_STATUS FROM TBL_MENU";
+
+        //when
+        Statement stmt = con.createStatement();
+        ResultSet rset = stmt.executeQuery(query);
+
+        List<Menu> menuList = new ArrayList<>();
+
+        while ((rset.next())) {
+            Menu menu = new Menu();
+            menu.setMenuCode(rset.getInt("MENU_CODE"));
+            menu.setMenuName(rset.getString("MENU_NAME"));
+            menu.setMenuPrice(rset.getInt("MENU_PRICE"));
+            menu.setCategoryCode(rset.getInt("CATEGORY_CODE"));
+            menu.setOrderableStatus(rset.getString("ORDERABLE_STATUS"));
+
+            menuList.add(menu);
+        }
+
+        //then
+        Assertions.assertTrue(!menuList.isEmpty());
+        menuList.forEach(System.out::println);
+
+        rset.close();
+        stmt.close();
+    }
+
+
+    @DisplayName("직접 SQL을 작성하여 신규메뉴를 추가할 때 발생하는 문제 확인")
+    @Test
+    void testDirectInsertSQL() throws SQLException {
+
+        //given
+
+        Menu menu = new Menu();
+        menu.setMenuName("민초");
+        menu.setMenuPrice(12000);
+        menu.setCategoryCode(1);
+        menu.setOrderableStatus("Y");
+
+        String query = "INSERT INTO TBL_MENU(MENU_NAME, MENU_PRICE, CATEGORY_CODE, " +
+                "ORDERABLE_STATUS) VALUE (?,?,?,?)";
+
+        //when
+        PreparedStatement psmt = con.prepareStatement(query);
+        psmt.setString(1, menu.getMenuName());
+        psmt.setInt(2, menu.getMenuPrice());
+        psmt.setInt(3, menu.getCategoryCode());
+        psmt.setString(4, menu.getOrderableStatus());
+
+        int result = psmt.executeUpdate();
+
+        //then
+        Assertions.assertEquals(1, result);
+        psmt.close();
+    }
+
+
+    /* 설명. 2. sql에 의존하여 개발 */
+    /* 설명. 2-1. 조회 항목 변경에 따른 의존성 확인 */
+    @DisplayName("조회항목 변경에 따른 의존성 확인")
+    @Test
+    void testChangeSelectColumns() throws SQLException {
+
+        //given
+        String query = "SELECT MENU_CODE, MENU_NAME FROM TBL_MENU";
+
+        //when
+        Statement stmt = con.createStatement();
+        ResultSet rset = stmt.executeQuery(query);
+
+        List<Menu> menuList = new ArrayList<>();
+        while ((rset.next())) {
+            Menu menu = new Menu();
+            menu.setMenuCode(rset.getInt("MENU_CODE"));
+            menu.setMenuName(rset.getString("MENU_NAME"));
+//            menu.setMenuPrice(rset.getInt("MENU_PRICE")); // 지운다면 쿼리도 같이 변경 시킴
+
+            menuList.add(menu);
+        }
+
+        //then
+        Assertions.assertNotNull(menuList);
+        menuList.forEach(System.out::println);
+
+        rset.close();
+        stmt.close();
+    }
+
+    /* 설명. 2-2. 연관된 객체 문제 */
+    @DisplayName("연관된 객체 문제 확인")
+    @Test
+    void testAssociationObject() throws SQLException {
+
+        //given
+        String query = "SELECT A.MENU_CODE, A.MENU_NAME, A.MENU_PRICE, B.CATEGORY_CODE, " +
+                "B.CATEGORY_NAME, A.ORDERABLE_STATUS " +
+                "FROM TBL_MENU A " +
+                "JOIN TBL_CATEGORY B ON (A.CATEGORY_CODE = B.CATEGORY_CODE)";
+
+        //when
+        Statement stmt = con.createStatement();
+        ResultSet rset = stmt.executeQuery(query);
+
+        List<MenuAndCategory> menuAndCategories = new ArrayList<>();
+        while (rset.next()) {
+            MenuAndCategory menuAndCategory = new MenuAndCategory();
+            menuAndCategory.setMenuCode(rset.getInt("MENU_CODE"));
+            menuAndCategory.setMenuName(rset.getString("MENU_NAME"));
+            menuAndCategory.setMenuPrice(rset.getInt("MENU_PRICE"));
+            menuAndCategory.setCategory(new Category(rset.getInt("CATEGORY_CODE"),
+                    rset.getString("CATEGORY_NAME")));
+            menuAndCategory.setOrderableStatus(rset.getString("ORDERABLE_STATUS"));
+
+            menuAndCategories.add(menuAndCategory);
+        }
+
+        //then
+        Assertions.assertNotNull(menuAndCategories);
+        menuAndCategories.forEach(System.out::println);
+
+        rset.close();
+        stmt.close();
+    }
+
+    /* 설명. 3. 패러다임 불일치(상속, 연관관계, 객체 그래프 탐색, 방향성) */
+}
