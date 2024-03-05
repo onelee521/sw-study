e9db45c728f4ccdf3e7336240ddd27683bf486ce
oneye <onelee521@gmail.com>
2024-03-05 12:37:37 +0900

    로그인 성공시 토큰 생성

--- 09_msa/chap04-01-user-service_lecture-source/src/main/java/com/ohgiraffers/userservice/security/AuthenticationFilter.java
+++ 09_msa/chap04-01-user-service_lecture-source/src/main/java/com/ohgiraffers/userservice/security/AuthenticationFilter.java
@@ -12,6 +12,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import java.io.IOException;
@@ -54,6 +55,10 @@ public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
                                             HttpServletResponse response,
                                             FilterChain chain,
                                             Authentication authResult) throws IOException, ServletException {
-        System.out.println("authResult = " + authResult);
+//        System.out.println("authResult = " + authResult);
+        String userName = ((User) authResult.getPrincipal()).getUsername();
+
+        System.out.println("시크릿 키: " + environment.getProperty("Token.secret"));
+        System.out.println("userName = " + userName);
     }
 }
