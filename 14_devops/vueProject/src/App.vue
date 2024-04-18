7a0a76dc84ba3c2d477efb4591bd825fee232e40
oneye <onelee521@gmail.com>
2024-04-18 09:32:23 +0900

    데브옵스 젠킨스 추가

---
+++ 14_devops/vueProject/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="plus">
+    <h1>덧셈 기능 만들기</h1>
+    <label>num1: </label><input type="text" v-model="num1">&nbsp;
+    <label>num2: </label><input type="text" v-model="num2">&nbsp;
+    <button @click="sendPlus">더하기</button>
+    <hr>
+    <p>`{{ num1 }} + {{ num2 }} = {{ result }}`</p>
+  </div>
+</template>
+
+<script setup>
+  import {ref} from 'vue';
+
+  const num1 = ref(0);
+  const num2 = ref(0);
+  const result = ref(0);
+
+  const sendPlus = async() => {
+    // const response = await fetch(`http://localhost:7777/plus?num1=${num1.value}&num2=${num2.value}`);
+    // const response = await fetch(`http://localhost:5173/api/plus?num1=${num1.value}&num2=${num2.value}`);
+
+    /* 백엔드를 도커 컨데이너로 8055 포트로 만들었을 때 */
+    // const response = await fetch(`http://localhost:8055/plus?num1=${num1.value}&num2=${num2.value}`);
+
+    /* 도커 컨테이너 간에 네트워트 연결 후 */
+    // const response = await fetch(`http://localhost:8011/api/plus?num1=${num1.value}&num2=${num2.value}`);
+
+    /* 백엔드를 쿠버네티스를 활용한 워커노드를 띄웠을 경우 (30001번 NodePort) */
+    const response = await fetch(`http://localhost:30001/plus?num1=${num1.value}&num2=${num2.value}`);
+
+    const data = await response.json();
+    result.value = data.sum;
+  }
+</script>
+
+<style scoped>
+  .plus {
+    text-align: center;
+  }
+</style>
---
+++ 14_devops/vueProject/vite.config.js
@@ -0,0 +1,33 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+
+  server: {
+    proxy: {
+      '/api' : {
+        // target: "http://localhost:7777",
+
+        /* 백엔드 프로젝트를 도커 컨테이너(8055:7777)로 바꾼 이후 */
+        // target: "http://localhost:8055",
+
+        /* 프론트 프로젝트(Vue)를 도커 컨테이너(8011:5173)로 바꾸고 도커 네트워크까지 연결 이후 */
+        target: "http://springboot-app:7777",
+
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  }
+})
