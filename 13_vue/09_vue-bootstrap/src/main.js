04c7a97596d2a5436f878df51b3c8b5c93381e86
oneye <onelee521@gmail.com>
2024-04-18 09:28:59 +0900

    vue 부트스트랩 추가

---
+++ 13_vue/09_vue-bootstrap/src/main.js
@@ -0,0 +1,14 @@
+
+import { createApp } from 'vue'
+import App from './App.vue'
+
+// 임포트 순서 중요
+import { BootstrapVue3 } from 'bootstrap-vue-3'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
+
+// createApp(App).mount('#app')
+const app = createApp(App)
+app.use(BootstrapVue3)
+app.mount('#app')
